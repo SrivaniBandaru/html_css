d415e73b60e15a089312cda4fd4122be19f1e6d0
SrivaniBandaru <srivanibandaru23@gmail.com>
2024-05-02 10:44:34 +0530

    updated

--- JavaScript/code/math/script.js
+++ JavaScript/code/math/script.js
@@ -9,4 +9,4 @@ var b = +prompt("enter b value");
 var c = Number(prompt("enter c value"));
 var d = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
 var e = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
-alert("results are "+ d +", "+e)
+alert("results are "+ d +", "+e) 
