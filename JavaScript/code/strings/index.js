7090dd5d07aaf60373831c064fa2ee9c05f3c857
SrivaniBandaru <srivanibandaru23@gmail.com>
2024-05-03 08:33:14 +0530

    updated

--- JavaScript/code/strings/index.js
+++ JavaScript/code/strings/index.js
@@ -8,8 +8,24 @@ var str2= "the cost of item is "+cost+"and something"+cost+"something";
 console.log(s, s2);
 console.log(str)
 console.log(str2)
-var strr = "Javascript"
+/*          0123456789  */
+var strr = "Javascript and html"
 console.log(strr.toUpperCase())
+console.log(strr.split())
+console.log(strr.split(''))
+console.log(strr.split(' '))
+console.log(strr.slice(1,5))
+console.log(strr.substring(4,9))
+console.log(strr.substring(2,10))
+console.log(strr.endsWith('x'))
+console.log(strr.startsWith('J'))
+//console.log(strr.join('s'))
+console.log(strr.indexOf('s'))
+console.log(strr.lastIndexOf('a'))
+console.log(strr.charAt(5))
+
+
+
 /* write a js code to explore all string methods */
 alert('are you sure')
 confirm('are you sure')
